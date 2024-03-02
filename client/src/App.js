4eadcaa8f651fe3ed18af91b89eb6ddea3eba8f9
杨皓钦 <yanghaoqin@yanghaoqindeMacBook-Air.local>
2024-03-02 04:17:03 -0500

    add frontend login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import React, { Fragment } from "react";
-const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
-);
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={<Landing />}
+        />
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
